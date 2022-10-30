0160a40c16e4fabc56aaaa82857fa774e36493c2
john <jinyngg@gmail.com>
2022-10-30 15:50:26 +0900

    type : 콜라문제 lv.1

---
+++ Programmers/콜라문제.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class 콜라문제 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @param n
+     * @return
+     */
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        return answer;
+    }
+}
