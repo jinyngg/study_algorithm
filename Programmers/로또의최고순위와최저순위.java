644756fcff3e3998aa093562a11ef66727fa5399
john(Jinyoung Jang) <jinyngg@gmail.com>
2022-10-31 15:06:42 +0900

    programmers : 로또의최고순위와최저순위 (test)

--- Programmers/로또의최고순위와최저순위.java
+++ Programmers/로또의최고순위와최저순위.java
@@ -12,7 +12,7 @@ public class 로또의최고순위와최저순위 {
 		int[] lottos = {44, 1, 0, 0, 31, 25};
 		int[] win_nums = {31, 10, 45, 1, 6, 19};
 		solution(lottos, win_nums);
-	}
+	} 
 	
 	/**
 	 * @date 2022-10-31
