13287a137b510484482dacfb25aafc9799935236
john <jinyngg@gmail.com>
2022-12-07 20:27:58 +0900

    programmers : 올바른 괄호 lv.2
    정확성: 69.5
    효율성: 15.2
    합계: 84.8 / 100.0

---
+++ programmers/올바른괄호.java
@@ -0,0 +1,90 @@
+package programmers;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 올바른괄호 {
+
+    public static void main(String[] args) {
+//        String s = "(())()";
+        String s = "()))((()"; // 테스트 케이스 5, 11, 18 유형
+//        String s = "(()(";
+        solution(s);
+
+    }
+
+    /**
+     *
+     *
+     * @param s '(' 또는 ')' 로만 이루어진 문자열 s
+     * @return 문자열 s가 올바른 괄호이면 true
+     */
+    static boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        char[] characters = s.toCharArray();
+
+        if (s.length()%2 == 1) {
+            return false;
+        }
+
+        for(char c : characters) {
+            if(stack.isEmpty()) {
+                if(c != '(') {
+                    return false;
+                }
+                stack.push(c);
+                continue;
+            }
+
+            if(stack.peek() == c) {
+                stack.push(c);
+                continue;
+            }
+            stack.pop();
+        }
+
+        if(stack.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+//    static boolean solution(String s) {
+//
+//        char[] characters = s.toCharArray();
+//        char left = '(';
+//        char right = ')';
+//        Stack<Character> stack = new Stack<>();
+//
+//        if (s.length()%2 == 1 || characters[0] == right) {
+//            return false;
+//        }
+//
+//        int index = 1;
+//        for(char c : characters) {
+//            if(!stack.isEmpty()) {
+//                if(stack.peek() != c){
+//                    stack.pop();
+//                    System.out.println("(pop) c : " + String.valueOf(c) + " || " + stack);
+//                }
+//                else {
+//                    stack.push(c);
+//                    System.out.println("(push) c : " + String.valueOf(c) + " || " + stack);
+//                }
+//                continue;
+//            }
+//            stack.push(c);
+//            System.out.println("(push) c : " + String.valueOf(c) + " || " + stack);
+//        }
+//
+//        System.out.println(stack);
+//
+//        if(stack.isEmpty()) {
+//            return true;
+//        }
+//
+//        return false;
+//    }
+}
